dded4bab22fa7bc5acdd067ffe2b550f15d3c7dc
SubekoPio <155113648+SubekoPio@users.noreply.github.com>
2024-11-07 13:11:39 +0300

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "hello <br>\n";
+    echo "date is:" .date('d-m-y, h:m:s')
+
+    ?>
+    
+</body>
+</html>
